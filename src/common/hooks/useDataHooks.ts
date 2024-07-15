13da682b4498aa5fd7a0fc86fe296680708a20b8
JaehoLee <chosloo2835@gmail.com>
2024-07-15 11:23:13 +0900

    Update useDataHooks.ts

--- src/common/hooks/useDataHooks.ts
+++ src/common/hooks/useDataHooks.ts
@@ -8,15 +8,8 @@ export const useSWRFetch = <Data = any, Error = any>(
   url: string,
   fetcher: FetcherFn<Data>,
   options?: SWRConfiguration<Data, Error>
-): Omit<SWRResponse<Data, Error>, 'isLoading'> => {
-  const { data, error, isValidating, mutate } = useSWR<Data, Error>(url, fetcher, options)
-
-  return {
-    data,
-    error,
-    isValidating,
-    mutate
-  }
+): SWRResponse<Data, Error> => {
+  return useSWR<Data, Error>(url, fetcher, options)
 }
 
 /** useSWRImmutable 전용 데이터 패칭 훅 */
@@ -24,13 +17,6 @@ export const useSWRImmutableFetch = <Data = any, Error = any>(
   url: string,
   fetcher: FetcherFn<Data>,
   options?: SWRConfiguration<Data, Error>
-): Omit<SWRResponse<Data, Error>, 'isLoading'> => {
-  const { data, error, isValidating, mutate } = useSWRImmutable<Data, Error>(url, fetcher, options)
-
-  return {
-    data,
-    error,
-    isValidating,
-    mutate
-  }
+): SWRResponse<Data, Error> => {
+  return useSWRImmutable<Data, Error>(url, fetcher, options)
 }

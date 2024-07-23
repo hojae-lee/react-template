8782b54e944c21fa496da28e5ac72e9a2b3ddde8
hojae-lee <chosloo2835@gmail.com>
2024-07-23 21:44:47 +0900

    update : vitest + react testing lib 예제 추가

---
+++ src/test/Button.test.tsx
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react'
+
+import Button from '@/common/components/Button.tsx'
+
+describe('Button Component Test', () => {
+  it('Button Component GUI Test', () => {
+    render(<Button>제출</Button>)
+    const button = screen.getByRole('button')
+    expect(button).toHaveTextContent('제출')
+  })
+})
--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,38 @@
-import { defineConfig } from 'vite'
+import { defineConfig, UserConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
+import { UserConfig as VitestUserConfig } from 'vitest/config'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-      '@assets': path.resolve(__dirname, './src/assets'),
-      '@common': path.resolve(__dirname, './src/common'),
-      '@layouts': path.resolve(__dirname, './src/layouts'),
-      '@entities': path.resolve(__dirname, './src/entities')
-    }
-  },
-  // scss 전역 사용
-  css: {
-    postcss: './postcss.config.cjs',
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/assets/styles/scss/main.scss";`
+interface CustomUserConfig extends UserConfig {
+  test?: VitestUserConfig['test']
+}
+
+export default defineConfig(({}): CustomUserConfig => {
+  const config: CustomUserConfig = {
+    plugins: [react()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+        '@assets': path.resolve(__dirname, './src/assets'),
+        '@common': path.resolve(__dirname, './src/common'),
+        '@layouts': path.resolve(__dirname, './src/layouts'),
+        '@entities': path.resolve(__dirname, './src/entities')
       }
+    },
+    css: {
+      postcss: './postcss.config.cjs',
+      preprocessorOptions: {
+        scss: {
+          additionalData: `@import "./src/assets/styles/scss/main.scss";`
+        }
+      }
+    },
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts'
     }
   }
+
+  return config
 })

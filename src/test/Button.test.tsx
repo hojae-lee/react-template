a2b9f2871a29870a00e8c174aaab840b142e9d3c
JaehoLee <chosloo2835@gmail.com>
2024-07-25 12:40:52 +0900

    fix : it -> test

--- src/test/Button.test.tsx
+++ src/test/Button.test.tsx
@@ -3,7 +3,7 @@ import { render, screen } from '@testing-library/react'
 import Button from '@/common/components/Button.tsx'
 
 describe('Button Component Test', () => {
-  it('Button Component GUI Test', () => {
+  test('Button Component GUI Test', () => {
     render(<Button>제출</Button>)
     const button = screen.getByRole('button')
     expect(button).toHaveTextContent('제출')

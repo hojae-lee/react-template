182124a390f75119e47b776615a2f19013d33aa6
hojae-lee <chosloo2835@gmail.com>
2024-08-18 14:28:35 +0900

    fix : msw 에서도 같은 url 쓰도록 변경

--- src/mocks/handlers.js
+++ src/mocks/handlers.js
@@ -1,8 +1,12 @@
 // src/mocks/handlers.js
 import { http, HttpResponse } from 'msw'
 
+import { getUsers } from '@/api/home/api.ts'
+
+const { url } = getUsers()
+
 export const handlers = [
-  http.get('/api/users', () => {
+  http.get(url, () => {
     // ...and respond to them using this JSON response.
     return HttpResponse.json({
       id: '1',
